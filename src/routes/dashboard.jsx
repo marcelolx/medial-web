f9ba15a2adea68568a8487d1a5982d5a77427695
Jonas Halmenschlager <jonassjo@outlook.com>
2018-10-24 22:10:24 -0300

    Ajustado rotas para permitir exibicao fora da dashboard

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -8,6 +8,7 @@ import Mediacao from "./../scenes/Register/Mediacao"
 import ListUsers from "../scenes/admin/ListUsers"
 import Profile from "./../scenes/Profile"
 import Configurations from "../scenes/admin/Configurations";
+import CadastroPendente from "../scenes/admin/mediacao/CadastroPendente";
 
 
 var dashRoutes = [
@@ -73,7 +74,15 @@ var dashRoutes = [
     nivel:[1,4,99],
     component: Profile
   },
-  { redirect: true, path: "/",   nivel:[1,4,99], pathTo: "/dashboard", name: "Dashboard" }
+  { 
+    path: "/mediacao/cadastropendente/:id",
+    name: "Mediacao",
+    icon: People,
+    nivel:[1,2],
+    naoExibe: true,
+    component: CadastroPendente
+  },
+  { redirect: true, path: "/",   nivel:[1,4,99], pathTo: "/dashboard", name: "Dashboard" },
 ];
 export default dashRoutes;
 
